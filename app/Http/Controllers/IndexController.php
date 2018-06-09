84887b30aa3c4ea2c442dc232c1124f761df8001
moqiaoduo <415372363@qq.com>
2018-06-09 13:56:33 +0800

    developing post list

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class IndexController extends Controller
 {
     public function index(){
-        return view('index');
+        $data=Post::paginate(10);
+        return view('index')->with('data',$data);
     }
 }
--- resources/views/default/index.blade.php
+++ resources/views/default/index.blade.php
@@ -1,23 +1,22 @@
 @extends('layout')
 
 @section('content')
+    <link rel="stylesheet" href="{{theme('css/list.css')}}">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
+                @foreach($data as $v)
+                    <div class="card">
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        You are logged in!
+                        <div class="card-body">
+                            <h1>{{$v['title']}}</h1>
+                        </div>
                     </div>
-                </div>
+                @endforeach
+
+                    {{ $data->links() }}
             </div>
+
         </div>
     </div>
 @endsection
