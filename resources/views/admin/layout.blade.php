a8117203cc540df534f739de0fe5e68766f6ddc7
moqiaoduo <415372363@qq.com>
2018-06-30 22:29:34 +0800

    一些小改动

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="robots" content="noindex, nofollow">
     <title>@yield('title') - {{config('app.name','Laraword')}} - Laraword</title>
     <!-- Bootstrap core CSS-->
     <link href="{{vendor('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
--- resources/views/default/layout.blade.php
+++ resources/views/default/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="{{env('APP_DESCRIPTION')}}">
+    <meta name="keywords" content="{{env('APP_KEYWORD')}}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
